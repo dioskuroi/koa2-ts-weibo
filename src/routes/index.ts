f41a42037c007a1af6881586571322f3aad243a9
徐俊 <xujun1019@hotmail.com>
2020-01-12 22:19:33 +0800

    init project

---
+++ src/routes/index.ts
@@ -0,0 +1,26 @@
+import Router from 'koa-router'
+const router = new Router()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+
+module.exports = router
