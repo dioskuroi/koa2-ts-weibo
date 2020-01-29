0b50adf7cddee7ab910bbcda2a2f330c273dabe0
徐俊 <xujun1019@hotmail.com>
2020-01-29 22:11:09 +0800

    feat: 个人主页 view

--- src/routes/view/blog.ts
+++ src/routes/view/blog.ts
@@ -9,27 +9,40 @@ import { UserInfo } from 'src/types'
 
 const router = new Router()
 
-interface IndexParam {
-  blogData: {
-    blogList: any[]
+
+interface UserData {
+  userInfo: UserInfo,
+  fansData: {
+    count: number,
+    list: any[]
   },
-  userData: {
-    userInfo: UserInfo,
-    fansData: {
-      count: number,
-      list: any[]
-    },
-    followersData: {
-      count: number,
-      list: any[]
-    }
-  }
+  followersData: {
+    count: number,
+    list: any[]
+  },
+  amIFollowed?: boolean
+}
+
+interface BlogData {
+  isEmpty: boolean,
+  blogList: any[],
+  pageSize: number,
+  pageIndex: number,
+  count: number
+}
+interface IndexParam {
+  blogData:BlogData,
+  userData: UserData
 }
 
 router.get('/', loginRedirect, async (ctx, next) => {
   const indexParam: IndexParam = {
     blogData: {
-      blogList: []
+      isEmpty: true,
+      blogList: [],
+      pageIndex: 0,
+      pageSize: 0,
+      count: 0
     },
     userData: {
       userInfo: ctx.session.userInfo,
@@ -44,6 +57,41 @@ router.get('/', loginRedirect, async (ctx, next) => {
     }
   }
   await ctx.render('index', indexParam)
+})
+
+interface ProfileParam {
+  blogData: BlogData,
+  userData: UserData
+}
+
+router.get('/profile', loginRedirect, async (ctx, next) => {
+  const { userName } = ctx.session.userInfo
+  ctx.redirect(`/profile/${userName}`)
+})
+
+router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
+  const ProfileParam: ProfileParam = {
+    blogData: {
+      isEmpty: true,
+      blogList: [],
+      pageIndex: 0,
+      pageSize: 0,
+      count: 0
+    },
+    userData: {
+      userInfo: ctx.session.userInfo,
+      fansData: {
+        count: 0,
+        list: []
+      },
+      followersData: {
+        count: 0,
+        list: []
+      },
+      amIFollowed: false
+    },
+  }
+  await ctx.render('profile', ProfileParam)
 })
 
 export default router
